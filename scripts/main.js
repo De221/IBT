c5c5d3c924ad2bc30875f73354a6c7d664681517
De221 <denits221@abv.bg>
2022-03-05 21:18:33 +0200

    My first git commit... yeaaa

---
+++ scripts/main.js
@@ -0,0 +1,49 @@
+/*const myHeading = document.querySelector("h1");
+myHeading.textContent = "My cat has an enormous dick";
+
+let myVariable;
+function multiply(num1, num2) {
+    let result = num1 * num2;
+    return result;
+}
+document.querySelector('h1').addEventListener('click', function() {
+    alert('Ouch! Stop poking me!');
+  });
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/shirt-text.png') {
+      myImage.setAttribute('src','images/FILE1381.jpg');
+    } else {
+      myImage.setAttribute('src','images/shirt-text.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading1 = document.querySelector('h2');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+        setUserName();
+    }
+    else {
+        localStorage.setItem('name', myName);
+        myHeading1.textContent = 'Zdravei, ' + myName + '.Ti si pulen gei.';
+    }  
+  }
+
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading1.textContent = 'Zdravei, ' + storedName + '.Ti si pulen gei.';
+  }
+
+  myButton.addEventListener('click', setUserName);*/
+//alert("pokazva gore izkachasht prozorec");
+//[1,2,3] - masiv
+//{key1: 1, key2: 2} = obekt v JS, a v PHP go narichat masiv
+// === e sravnenie
